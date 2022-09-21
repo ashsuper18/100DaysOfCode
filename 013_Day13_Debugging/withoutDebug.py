4aec5c0780078fa688e22a9d3e342850f7520f68
ak7853 <ashsuper18@gmail.com>
2022-09-21 13:40:49 +0530

    Day13

--- 013_Day13_Debugging/withoutDebug.py
+++ 013_Day13_Debugging/withoutDebug.py
@@ -24,7 +24,7 @@
 # elif year > 1994:
 #     print("You are a Gen Z.")
 
-# # Fix the Errors
+# Fix the Errors
 # age = input("How old are you?")
 # if age > 18:
 # print("You can drive at age {age}.")
